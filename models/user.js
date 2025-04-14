f6f18fa128d13b3f0aaab27272d2a263ece8b131
KRISHNA BAROCHIA <125182806+Dragon-KJB@users.noreply.github.com>
2025-04-14 22:08:27 +0530

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['customer', 'provider'],
+        default: 'customer'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
