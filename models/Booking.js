721bc95b9692ca580a54d23ce0559a01104fcf5b
KRISHNA BAROCHIA <125182806+Dragon-KJB@users.noreply.github.com>
2025-04-14 22:08:45 +0530

    Add files via upload

---
+++ models/Booking.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const BookingSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    service: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
